dcf1419bf9d653bcb33ad8a326a784b107c674a5
bleepbloopsify <bleepbloopsify@gmail.com>
2015-11-07 22:11:14 -0500

    Workin on Maps and Markers

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import user
 
-
+API_KEY="AIzaSyDbrIKnZ-fJcUxd636duQL8khuiekjC5pQ"
 
 app = Flask(__name__)
-
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template("Home.html")
- 
+
+@app.route('/map')
+def mapPage():
+    coords =[
+    {"latitude":100,"longitude":100},
+    {"latitude":200,"longitude":100},
+    {"latitude":300,"longitude":100},
+    {"latitude":400,"longitude":100},
+    {"latitude":500,"longitude":100},
+    {"latitude":-100,"longitude":100},
+    {"latitude":-200,"longitude":100},
+    ]
+
+    return render_template("map.html", coords=coords, API_KEY=API_KEY )
+
 
 if __name__ == "__main__":
     app.debug = True
