05df45a55901fab38aa4ac4381f6ac34e192a7b1
Nicholas Yang <nick@nicholasyang.com>
2015-11-07 22:14:58 -0500

    added basic latitude longitude retrieval

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import user
+import urllib2, json
 
 
 
@@ -9,9 +10,23 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("Home.html")
- 
+    return render_template("home.html")
 
+@app.route('/test')
+def getFlickrInfo():
+    import urllib2, json
+    method = 'flickr.photos.geo.getLocation'
+    photoid = '21870769994'
+    key = '20dd8b0f53a96c73c31c2f9ec7a22c9f'
+    uri = 'https://api.flickr.com/services/rest/?format=json&nojsoncallback=1&method=%s&api_key=%s&photo_id=%s'
+    url = uri%(method, key, photoid)
+    request = urllib2.urlopen(url)
+    result = request.read()
+    translated = json.loads(result)
+    longitude = translated['photo']['location']['longitude']
+    latitude =  translated['photo']['location']['latitude']        
+    return render_template("location.html", longitude = longitude, latitude=latitude)
+    
 if __name__ == "__main__":
     app.debug = True
     app.secret_key = "gP_3.hV[KE-P@|{pE5+Iv+m}"
--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import urllib2, json
-method = 'flickr.activity.userComments'
-uri = 'https://api.flickr.com/services/rest/?method=%s'
-url = uri%(method)
-
-print url
+method = 'flickr.photos.geo.getLocation'
+photoid = '21870769994'
+key = '20dd8b0f53a96c73c31c2f9ec7a22c9f'
+uri = 'https://api.flickr.com/services/rest/?format=json&nojsoncallback=1&method=%s&api_key=%s&photo_id=%s'
+url = uri%(method, key, photoid)
+request = urllib2.urlopen(url)
+result = request.read()
+# print result
+translated = json.loads(result)
+print translated['photo']['location']['longitude']
